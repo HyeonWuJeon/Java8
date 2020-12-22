0159bb84b30fea6b75cb7218dfde1fc26c1e0e1d
HyeonWuJeon <tkawnd111@gmail.com>
2020-12-22 22:52:15 +0900

    함수형 인터페이스 예제 클래스 출력 변경

--- src/main/java/com/example/deinterface/DefaultFoo.java
+++ src/main/java/com/example/deinterface/DefaultFoo.java
@@ -2,7 +2,7 @@ package com.example.deinterface;
 
 
 
-public class DefaultFoo implements Foo, Bar { // 다이아몬드 문제 , 어떤default를 먼저 쓰게 할거냐? 컴파일 에러
+public class DefaultFoo implements Foo, Bar{ // 다이아몬드 문제
 
 
     String name;
@@ -16,15 +16,25 @@ public class DefaultFoo implements Foo, Bar { // 다이아몬드 문제 , 어떤
         System.out.println(name);
     }
 
-    //충돌하는 디폴트 메소드가 있는 경우 직접 오버라이드 해야한다.
     @Override
     public void printNameUpperCase() {
-        System.out.println(getName().toUpperCase());
+
     }
 
     @Override
     public String getName() {
-        return name;
+        return null;
     }
+
+    //충돌하는 디폴트 메소드가 있는 경우 직접 오버라이드 해야한다.
+//    @Override
+//    public void printNameUpperCase() {
+//        System.out.println(getName().toUpperCase());
+//    }
+
+//    @Override
+//    public String getName() {
+//        return name;
+//    }
 
 }
--- src/main/java/com/example/deinterface/Foo.java
+++ src/main/java/com/example/deinterface/Foo.java
@@ -5,9 +5,10 @@ public interface Foo {
 
     /**
      * @implSpec 이 구현체는 getName()으로 가져온 문자열을 대문자로 바꿔 출력한다.
+     *
      */
     default void printNameUpperCase(){
-        System.out.println(getName().toUpperCase());
+        System.out.println("bar");
     }
 
 //    default String toString(){ default 메소드는 object것들을 재정의 할 수 없다.
