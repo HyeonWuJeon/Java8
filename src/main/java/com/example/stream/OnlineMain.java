923b02fa26a6825937cf7b8628b9717ddf97afae
HyeonWuJeon <tkawnd111@gmail.com>
2021-01-30 03:21:53 +0900

    lambda flatmap

--- src/main/java/com/example/stream/OnlineMain.java
+++ src/main/java/com/example/stream/OnlineMain.java
@@ -26,8 +26,10 @@ public class OnlineMain {
         hyunwooEvents.add(springClasses);
         hyunwooEvents.add(javaClasses);
 
+
         System.out.println("spirng 으로 시작하는 수업");
         //TODO
+
         springClasses.stream()
                 .filter(x -> x.getTitle().startsWith("spring"))
                 .forEach(x-> System.out.println(x.getTitle()));
@@ -40,6 +42,9 @@ public class OnlineMain {
         springClasses.stream()
                       .filter(Predicate.not(OnlineClass::isColsed))
                       .forEach(oc-> System.out.println(oc.getId()));
+        hyunwooEvents.stream().flatMap(Collection::stream)
+                .filter(Predicate.not(OnlineClass::isColsed))
+                .forEach(x-> System.out.println(x.getTitle()));
 
         System.out.println("스프링 클래스의 수업 이름만 모아서 스트림 만들기");
         //TODO
@@ -52,7 +57,6 @@ public class OnlineMain {
         hyunwooEvents.stream()
                 .flatMap(Collection::stream) // 두 클래스를 나열한다.
                 .forEach(oc-> System.out.println(oc.getTitle()));
-
         System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
         //TODO
         Stream.iterate(10, i-> i+1) // 중개형 오퍼레이터.
@@ -79,10 +83,8 @@ public class OnlineMain {
         //TODO
         List<String> stringList = springClasses.stream()
                                                 .map(OnlineClass::getTitle)
-                                                .sorted(Comparator.reverseOrder())
+                                                .sorted(Comparator.reverseOrder()) // natureOrder :: 기본비교 , reverseOrder :: 역비교
                                                 .collect(Collectors.toList());
         stringList.forEach(System.out::println);
     }
-
-
 }
